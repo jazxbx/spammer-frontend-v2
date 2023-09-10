d4d700ad370203f3bdba6deae06e4b752dd24de8
jazz <jazxbx@gmail.com>
2023-09-10 00:51:30 -0700

    edit msg fn added

--- src/Components/HandleEditMessage.jsx
+++ src/Components/HandleEditMessage.jsx
@@ -1,16 +1,40 @@
 import { useState } from "react";
-// import { API } from "../API";
+import { API } from "../API";
 
 function HandleEditMessage({ message, fetchMessages }) {
   // state if nag edit or wala
   const [editing, setEditing] = useState(false);
-  console.log(editing);
-  console.log(`after:`, editing);
+  //   state for input box
+  const [inputMessage, setInputMessage] = useState(message.text);
+
+  async function fetchEditMessage(e) {
+    e.preventDefault();
+
+    const res = await fetch(`${API}/messages/${message.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        text: inputMessage,
+      }),
+    });
+    const data = await res.json();
+    console.log(data);
+    setEditing(false);
+    fetchMessages();
+    setInputMessage("");
+  }
+
   return (
     <div>
       {editing ? (
-        <form action="">
-          <input type="text" />
+        <form key={message.id} onSubmit={fetchEditMessage}>
+          <input
+            type="text"
+            value={inputMessage}
+            onChange={(e) => setInputMessage(e.target.value)}
+          />
           <button>save</button>
           <button
             onClick={() => {
